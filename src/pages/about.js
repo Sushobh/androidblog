eec31883605a61d82c3b7653575af3e3b82db14c
Sushobh Nadiger <aryansushobh@gmail.com>
2020-10-09 22:38:10 +0530

    Changes

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,30 @@
 import React from "react"
-
+import Layout from "../components/layout"
 
 
 export default function About() {
   return (
-    <div>
-      <h1>AppContext</h1>
-      <p>
-        The Android developer blog.
-        </p>
-    </div>
-  )
+    <Layout >
+      <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
+      <br/>
+      <h2>Welcome to AppContext , the Android developer blog.</h2>
+      <br/>
+
+       <h4>Congratulations , you have found a greate place for 
+         quality content on Android development. 
+         We , the maintainers of this blog are on always on top of cutting edge
+         technologies and trends related to Android development.
+         We have years of experience building and publishing Android
+         apps and are obsessed with developing apps the right way.
+         Right from naming conventions to architecting large scale 
+         multi module apps we are laser focused on best practices.
+         All of this convinced us that it would be a great idea to 
+         share our knowledge and expereinces with the wider internet. 
+         Do hang around the blog for a little while, we are sure 
+         that you will find the content here engrossing. 
+       </h4>
+      </div>
+      
+    </Layout>
+  );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,21 +6,11 @@ deckDeckGoHighlightElement();
 
 export default function Home() {
   return (
-    <Layout>
-       <h2>Welcome to AppContext , the Android developer blog.</h2>
-       <h4>Congratulations , you have found a greate place for 
-         quality content on Android development. 
-         We , the maintainers of this blog are on always on top of cutting edge
-         technologies and trends related to Android development.
-         We have years of experience building and publishing Android
-         apps and are obsessed with developing apps the right way.
-         Right from naming conventions to architecting large scale 
-         multi module apps we are laser focused on best practices.
-         All of this convinced us that it would be a great idea to 
-         share our knowledge and expereinces with the wider internet. 
-         Do hang around the blog for a little while, we are sure 
-         that you will find the content here engrossing. 
-       </h4>
+    <Layout >
+      <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
+       
+      </div>
+      
     </Layout>
   );
 }
