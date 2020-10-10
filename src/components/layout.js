ccc8b136d3c6447554490c1a61d220e85c02f4a8
Sushobh Nadiger <aryansushobh@gmail.com>
2020-10-10 09:07:00 +0530

    Alright

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,7 @@
 import React from "react"
 import "../css/global.scss"
 import "../css/layout.scss"
+import { Link } from "gatsby"
 
 export default function Layout({ children }) {
   
@@ -10,6 +11,7 @@ export default function Layout({ children }) {
     <div>
       <div id="layout_header">
       <h1>AppContext</h1>
+      <Link to="/about" style={{color : "white",paddingRight : "20px",textDecoration : "none"}}><h3>About</h3></Link>
       </div>
       {children}
     </div>
